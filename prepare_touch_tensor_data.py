cad03e1017457fa1d8770dc1acb91f21ac74779d
OEmiliatanO <Emiliaistruelove@gmail.com>
2026-04-22 20:33:09 +0800

    update

--- prepare_touch_tensor_data.py
+++ prepare_touch_tensor_data.py
@@ -10,6 +10,9 @@ import numpy as np
 def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 set_seed(0)
 
