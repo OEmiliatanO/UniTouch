a418af7546157ef1d4c5945b30e820a3bd2e7572
OEmiliatanO <Emiliaistruelove@gmail.com>
2026-04-30 23:06:46 +0800

    update

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import random 
+import numpy as np
 import torch
 import torch.nn as nn
 from torchvision import transforms
@@ -11,3 +13,11 @@ def cka(feat_A, feat_B):
 def mknn(feat_A, feat_B, k=10):
     eval_fn = lambda x,y: AlignmentMetrics.measure("mutual_knn", x, y, topk=k)
     return calibrate(feat_A, feat_B, eval_fn)
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
